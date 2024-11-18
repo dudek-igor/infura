8ab379c052320d9a0030262fb2ee939bc55f5963
Igor Dudek <igor.dudek@firma.interia.pl>
2024-11-18 19:50:17 +0100

    feat: create ethers.js example

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const axios = require("axios");
-
-const infuraApiKey = process.env.INFURA_API_KEY;
-
+const { ethers } = require("ethers");
+/**
+ * @constans
+ */
+const ETH_MAINNET = `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`;
+/**
+ * @example Axios Example
+ */
 axios
-  .post(`https://mainnet.infura.io/v3/${infuraApiKey}`, {
+  .post(ETH_MAINNET, {
     jsonrpc: "2.0",
     method: "eth_blockNumber",
     params: [],
@@ -12,6 +17,18 @@ axios
   })
   .then((response) => {
     console.log(response);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+/**
+ * @example Ether Example
+ */
+const InfuraProvider = new ethers.JsonRpcProvider(ETH_MAINNET);
+
+InfuraProvider.getBlock()
+  .then((block) => {
+    console.log(block);
   })
   .catch((error) => {
     console.error(error);
